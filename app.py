1bff02adcab1a54c4eb3ecefd04cd37286418016
srirakesh.chaluvadi@gmail.com <rak12355@gmail.com>
2019-09-08 01:35:36 +0530

    added funtionality

--- app.py
+++ app.py
@@ -8,18 +8,20 @@ def index():
     if request.method == 'POST':
         file = request.files['file']
         extention = file.filename.split(".")[-1]
-        if extention == 'mp3' or extention == 'wav':
+        if extention == 'wav':
             r = sr.Recognizer()
             with sr.AudioFile(file) as source:
                 audio = r.record(source)
             try:
-                print("Content " + r.recognize_sphinx(audio))
+                #print("Content " + r.recognize_sphinx(audio))
+                data = r.recognize_google(audio)
+                return render_template('index.html', data=data)
             except sr.UnknownValueError:
                 print("could not understand audio")
             except sr.RequestError as e:
                 print("error; {0}".format(e))
         else:
-            return "Please upload audio file"
+            return render_template('index.html', alert = "Please upload '.wav' files only", isAlert = True)
     else:
         return render_template('index.html')
 
